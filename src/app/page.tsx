e74e0c0ea90f774ae52e42b6eb62f161f20bf296
Carson274 <carsonsecrest@gmail.com>
2024-03-23 20:01:56 -0700

    Added smooth scrolling from framer motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,16 @@ import Image from "next/image";
 import Hero from './sections/Hero/Hero';
 import About from './sections/About/About';
 import Projects from './sections/Projects/Projects';
+import SmoothScroll from "./components/SmoothScroll";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <Hero />
-      <About />
-      <Projects />
-    </main>
+    <SmoothScroll>
+      <main className="flex min-h-screen flex-col items-center justify-between bg-light-cream">
+        <Hero />
+        <About />
+        <Projects />
+      </main>
+    </SmoothScroll>
   );
 }
